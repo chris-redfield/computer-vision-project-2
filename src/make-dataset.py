fc8e43c12ed56558d78ef4f58b6b79393607b024
Christian <christianmoryah@gmail.com>
2021-05-09 16:18:07 -0300

    update make-datset

--- src/make-dataset.py
+++ src/make-dataset.py
@@ -1,80 +1,103 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# ### Import libs
+
+# In[1]:
 
 
 import json
 import pandas as pd
 import cv2
 import numpy as np
+import argparse
 
 
 # In[2]:
 
 
-data_path = 'deepfake-detection-challenge-data/'
-with open(f'{data_path}metadata.json') as json_file:
-    data = json.load(json_file)
+import os
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
+
+# ### Extract face frames from video files
 
 # In[3]:
 
 
-real_path = 'data/real/'
-fake_path = 'data/fake/'
+face_cascade = cv2.CascadeClassifier('models/haarcascades/haarcascade_frontalface_alt.xml')
+
+
+def detect_faces(frame):
+    frame_gray = cv2.equalizeHist(frame)
+    faces = face_cascade.detectMultiScale(frame_gray)
+    return faces
+ 
+def get_faces_from_video_file(file_path, fake):
+
+    cap = cv2.VideoCapture(file_path)
+    file_name = file_path.split('/')[-1]
+
+    frame_values = [80 ,100, 150, 200]
+    #frame_values = [200]
+
+    for frame_number in frame_values:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number);
+        ret, frame = cap.read()
+        
+        cv2.imshow(f'img-{frame_number}', frame)
+        
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = detect_faces(gray)
+        
+#         print(faces)
+#         print(len(faces))
+        
+        if faces is not None and len(faces) > 0:
+            for (x,y,w,h) in faces:
+                faceROI = frame[y:y+h,x:x+w]
+                cv2.imshow(f'face-{frame_number}', faceROI)
+                
+            ### gets only the last face found
+            #cv2.imshow(f'frame-{frame_number}', faceROI)
+            #cv2.imwrite(file_path.replace(".mp4","") + f'-f{frame_number}.jpg',frame)
+            cv2.imwrite(f'data/{fake}/{file_name}-f{frame_number}.jpg',faceROI)
+
+        else:
+            print(f'face not found for file {file_path}, frame {frame_number}')
+            
+    cv2.waitKey()
+
+
+# In[4]:
+
+
+data_path = 'deepfake-detection-challenge-data/'
+
+with open(f'{data_path}metadata.json') as json_file:
+    data = json.load(json_file)
+
+
+# In[5]:
+
+
+# real_path = 'data/real/'
+# fake_path = 'data/fake/'
 
 for key in data.keys():
-    print(key)
-    print(data[key])
+    file_name = key
+    fake = data[key]['label'].lower()
+    print('extracting images for file',file_name,':',fake)
+    get_faces_from_video_file(data_path + file_name, fake)
 
 
-# In[16]:
+# ### Single file test
+
+# In[5]:
 
 
-# # Captura o video
-# vid = cv2.VideoCapture(data_path + 'slwkmefgde.mp4') 
-# cap.get(cv2.CAP_PROP_POS_FRAMES)
-# while not vid.isOpened():
-#     vid = cv2.VideoCapture(data_path + 'slwkmefgde.mp4')
-#     cv2.waitKey(1000)
-#     print("Wait for the header")
-
-    
-# while(True):
-#     # Captura cada frame do video
-#     ret, frame = vid.read() 
-
-
-# In[15]:
-
-
-cap = cv2.VideoCapture(data_path + 'slwkmefgde.mp4')
-count = 0
-cap.get(cv2.CAP_PROP_POS_FRAMES)
-while cap.isOpened():
-    ret,frame = cap.read()
-    cv2.imshow('window-name', frame)
-    cv2.imwrite("frame%d.jpg" % count, frame)
-    count = count + 1
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-print('teste')
-
-
-# In[11]:
-
-
-data_path + 'slwkmefgde.mp4'
-
-
-# In[14]:
-
-
-cv2.__version__
+#get_faces_from_video_file(data_path + 'agqphdxmwt.mp4','fake')
 
 
 # In[ ]:
